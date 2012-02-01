12653d7a3970a337de94391b8315939f66d51b4a
Martin Hoffmann <m.hoffmann@isekinet.de>
2012-02-01 13:34:01 +0100

    Added docs

--- kirby/lib/plugin.php
+++ kirby/lib/plugin.php
@@ -3,10 +3,19 @@
 // direct access protection
 if(!defined('KIRBY')) die('Direct access is not allowed');
 
+/**
+ * Central Plugin class for registering and invoking Plugins 
+ */
 class Plugins {
 
 	private static $plugins = array();
 
+	/**
+	 * Register a new Plugin Class. Always use the method to register new plugins.
+	 *
+	 * @param Plugin $plugin
+	 *	Each plugin needs at least to implement the Plugin interface. Each plugin requires a unique name, otherwise it may be overridden by plugins with equal names.
+	 */
 	static function register(Plugin $plugin) {
 		if (!in_array($plugin, self::$plugins)) {
 			$req_version = $plugin->min_kirby_version();
@@ -19,6 +28,9 @@ class Plugins {
 		}
 	}
 	
+	/**
+	 * Returns a list of installed plugins
+	 */
 	static function installed() {
 		$result = array();
 		foreach (self::$plugins as $name => $plugin) {
@@ -27,6 +39,18 @@ class Plugins {
 		return $result;
 	}
 	
+	/**
+	 * Invokes Plugin functions. Always use this method to invoke plugins.
+	 *
+	 * @param $type
+	 *	The interface name of the Plugin
+	 * @param
+	 *	The function within the plugin class
+	 * @param
+	 *	Arguments to pass to the plugins
+	 * @return
+	 *	The results of all plugins as an hashmap having plugin names as keys and return values as values.
+	 */
 	static function invoke($type, $func, $args = array()) {
 		if (!interface_exists($type)) {
 			throw new Exception('Unkown Class Name: ' . $type);
@@ -45,12 +69,24 @@ class Plugins {
 	
 }
 
+/**
+ * The basic plugin interface. Use this to implement own plugin hooks.
+ */
 interface Plugin {
 
+	/**
+	 * Returns the name of the plugin
+	 */
 	public function name();
 	
+	/**
+	 * Returns the version of the plugin
+	 */
 	public function version();
 	
+	/**
+	 * Returns the minimum required Kirby version
+	 */
 	public function min_kirby_version();
 
 }
--- site/plugins/kirbytext_example.php
+++ site/plugins/kirbytext_example.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Example implementation of a the KirbyTextPlugin
+ *
+ * This plugin in creates a new wikipedia plugin, with the parameters language and text. Just as the example given on the getkirby.de website.
+ * 
+ * The plugin implements the methods name, version and min_kirby_version, which are required for all plugins.
+ *
+ * Additional the the method tagname returns the implemented tag name (yes, it is only one per class) and attributes tag names of that tag.
+ * The method parse_<tagname> implements the logic.
+ * 
+ * Finally, the Plugins::register() method registers the new plugin.
+ */
+
+
 class MyWikiKirbyTextPlugin implements KirbyTextPlugin {
 
 	function name() {
